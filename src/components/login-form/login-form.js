0941d9378ffaf047c4679f87b8ad7bf544b6e840
Espinosa <hrea@gft.com>
2021-09-17 12:13:03 +0200

    implementa autenticacion con firebase

--- src/components/login-form/login-form.js
+++ src/components/login-form/login-form.js
@@ -1,16 +1,42 @@
 import React from "react";
 import InputField from "../input-field/input-field";
-/*
-
-*/
+import CustomButton from "../button/button";
+//import  PropTypes from "prop-types";
 class LoginForm extends React.Component{
+
+    emailRef = React.createRef();
+    passwordRef = React.createRef(); 
+
+    login = (event) => {
+        event.preventDefault();  
+        console.log("doing login in form");
+        const tryingUser = {
+            email:this.emailRef.current.value,
+            password:this.passwordRef.current.value
+        }
+        this.props.doLogin(tryingUser);
+        event.currentTarget.reset();
+    }
+
     render() {
         return(
-                <form className="">
-                    <InputField type="text" id="userName" name="userName" placeholder="User name" required={true}/>
-                    <InputField type="password" id="password" name="password" placeholder="********" required={true}/>
+                <form className="" onSubmit={this.login}>
+                    <input ref={this.emailRef} type="email" name="" placeholder="Email" required/>
+                    <input ref={this.passwordRef} type="password" name="password" placeholder="********" required/>
+                    <input type="submit" name="loginSubmit"  value="Log in"  id="loginSubmit"/>
                 </form>
         )
     }
 }
+/*
+const isEmail = function(emailAdress) {
+    const regex = /^((([^<>()[]\.,;:s@"]+(.[^<>()[]\.,;:s@"]+)*)|(".+"))@(([[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}.[0-9]{1,3}])|(([a-zA-Z-0-9]+.)+[a-zA-Z]{2,})))?$/;
+    if (!regex.test(emailAdress)) {
+      return new Error("introduzca una dirección de correo válida");
+    }
+  }
+
+LoginForm.PropTypes = {
+
+}*/
 export default LoginForm;
--- src/index.js
+++ src/index.js
@@ -2,10 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import LoginForm from './components/login-form/login-form';
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth, createUserWithEmailAndPassword,signInWithEmailAndPassword } from "firebase/auth";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyAIJPOyPQLhf9fehpcF2HHpUmyW-aNcBiU",
   authDomain: "loginapp-9b3e6.firebaseapp.com",
@@ -15,15 +13,56 @@ const firebaseConfig = {
   appId: "1:751348744351:web:b825edc28aef725f4f22aa"
 };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
 
+const app = initializeApp(firebaseConfig);
+/*const auth = getAuth();
+createUserWithEmailAndPassword(auth, "hectorm21@live.com", "abc123")
+  .then((userCredential) => {
+    // sesion iniciada
+    console.log(userCredential.user);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+*/
 class App extends React.Component{
+  state = {
+    tryingUser:{
+    },
+    connectedUser:{
+    }
+  }
+  doLogin = tryingUser =>{
+
+    console.log(tryingUser);
+    const auth = getAuth();
+    signInWithEmailAndPassword(auth, tryingUser.email, tryingUser.password)
+      .then((userCredential) => {
+        console.log("sesion iniciada", userCredential.user);
+        console.log("state", this.state);
+        const user = userCredential.user;
+        // ...
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+      });
+
+
+
+
+  }
     constructor(){
         super()
-        /*this.state{
 
-        }*/
     }
+    render() {
+      return(
+        <LoginForm doLogin={this.doLogin}/>
+      )
+    }
+    
 }
-ReactDOM.render(<LoginForm/>, document.getElementById('main')); 
+
+
+ReactDOM.render(<App/>, document.getElementById('main')); 
