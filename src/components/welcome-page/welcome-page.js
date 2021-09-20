5053bca02ff65e30438dc46e40f079dd4fca375b
Espinosa <hrea@gft.com>
2021-09-20 16:29:35 +0200

    reloj se actualiza en tiempo real

--- src/components/welcome-page/welcome-page.js
+++ src/components/welcome-page/welcome-page.js
@@ -1,6 +1,4 @@
 import React from "react";
-import { LoginService } from '../../services/login-service';
-import { HelpersService } from '../../helpers/helpers';
 import DigitDisplay from '../digit-display/digit-display'
 import './welcome-page.css';
 import '../App/App.css';
@@ -11,19 +9,28 @@ class WelcomePage extends React.Component{
         event.preventDefault();  
         this.props.logOut();
     }
+ 
+    refreshClock = () => {
+        this.props.refreshClock()
+    }
 
-    lastSignIn = HelpersService.timeSinceLastSignin();
+    componentDidMount() {
+        setInterval( () => {
+            [this.refreshClock()]
+        },1000)
+    }
+
     render() {
         return(
             <React.Fragment>
-
+                
                 <p className="welcomeText">WELCOME!</p>
                 <p className="welcomeMessage">the last time you accesed was</p>
                 <div className="clockContainer">
-                    <DigitDisplay text="days" digits={this.lastSignIn.days}></DigitDisplay>
-                    <DigitDisplay text="hours" digits={this.lastSignIn.hours}></DigitDisplay>
-                    <DigitDisplay text="minutes" digits={this.lastSignIn.mins}></DigitDisplay>
-                    <DigitDisplay text="seconds" digits={this.lastSignIn.secs}></DigitDisplay>
+                    <DigitDisplay text="days" digits={this.props.time.days}></DigitDisplay>
+                    <DigitDisplay text="hours" digits={this.props.time.hours}></DigitDisplay>
+                    <DigitDisplay text="minutes" digits={this.props.time.mins}></DigitDisplay>
+                    <DigitDisplay text="seconds" digits={this.props.time.secs}></DigitDisplay>
                 </div>
          <div>
                     <button name="loginSubmit"
